c9708dbe0fa97be69daa97198ddcad56708e2885
DESKTOP-92TCAM3\lg <macoto8@naver.com>
2024-05-19 18:53:40 +0900

    계정 생성 구문

---
+++ 00_계정 생성(SYS 계정 접속).sql
@@ -0,0 +1,61 @@
+-- 한 줄 주석 (ctrl + /)
+
+/* 범위 주석 (ctrl + shift +/) */ 
+
+/* SQL 1개 실행 : ctrl + ENTER
+ * 
+ * SQL 여러 개 실행 : (블럭 후) ALT + X
+ */
+
+/* 계정 생성 구문*/
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+-- 계정 생성
+CREATE USER KH_LEJ IDENTIFIED BY KH1234;
+
+ -- 생성된 계정에
+-- 접속 + 기본 자원 관리 권한 추가
+GRANT CONNECT, RESOURCE TO KH_LEJ;
+
+-- 객체 생성 공간 할당
+ALTER USER KH_LEJ
+DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
+
+---
+
+
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+
+-- 계정 생성
+CREATE USER WORKBOOK IDENTIFIED BY KH1234;
+
+ -- 생성된 계정에
+-- 접속 + 기본 자원 관리 권한 추가
+GRANT CONNECT, RESOURCE TO WORKBOOK;
+
+-- 객체 생성 공간 할당
+ALTER USER WORKBOOK
+DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+
+-- SPRING_LEJ 계정 생성
+CREATE USER SPRING_LEJ IDENTIFIED BY SPRING1234;
+
+ -- 생성된 계정에
+-- 접속 + 기본 자원 관리 권한 추가
+GRANT CONNECT, RESOURCE TO SPRING_LEJ;
+
+-- 객체 생성 공간 할당
+ALTER USER SPRING_LEJ
+DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
